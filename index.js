b4d743db0080da97b4cc88b9e35ab26ecb6ee8bc
kingkor399 <farhanrahmanyou@gmail.com>
2022-11-28 01:06:39 +0600

    sixth commit

--- index.js
+++ index.js
@@ -121,6 +121,13 @@ async function run() {
             const result = await productsCollection.deleteOne(query);
             res.send(result);
         })
+
+        app.delete('/alluser/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
     }
 
     finally {
